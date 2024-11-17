32ccec06f8ca4e43e863064b76559534a0dfe519
Prabhu <vprabhu1771@gmail.com>
2024-11-17 21:04:55 +0530

    Bank Admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from backend.models import Bank
+
+
 # Register your models here.
+class BankAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code',)
+
+    search_fields = ('name', 'code',)
+
+admin.site.register(Bank, BankAdmin)

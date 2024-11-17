8b43c701ca12ea63bbef943e044ebd56e1ebb358
Prabhu <vprabhu1771@gmail.com>
2024-11-17 21:04:18 +0530

    Bank Model

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Bank(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255, unique=True)
+    code = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "bank"
